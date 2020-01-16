590f4274d9e337133fcf2760f6c6260fa5ca405e
SantiagoMoreira3C <56278999+SantiagoMoreira3C@users.noreply.github.com>
2020-01-16 14:56:47 -0500

    PatronSingleton

---
+++ Patron_Singleton/Patron_Singleton/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SingletonDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton fromTeachaer = Singleton.GetInstance;
+            fromTeachaer.PrintDetails("Del porefesor");
+            Singleton fromStudent = Singleton.GetInstance;
+            fromStudent.PrintDetails("Del estudiante");
+            Console.ReadLine();
+        }
+    }
+}
+
---
+++ Patron_Singleton/Patron_Singleton/Singlenton.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SingletonDemo
+{
+
+    // se crea una instancia llamada singleton
+    public sealed class Singleton
+    {
+        // se crean propiedades privadas
+        private static int contar = 0;
+        private static Singleton instance = null;
+        public static Singleton GetInstance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new Singleton();
+                return instance;
+            }
+        }
+         
+        private Singleton()
+        {
+            contar++;
+            Console.WriteLine("Contar valor " + contar.ToString());
+        }
+        public void PrintDetails(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
